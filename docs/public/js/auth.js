ec30eb18fb9d5c52d9203a26875c3db9119ea185
DdiazCutie <daviddiazcutie2000@gmail.com>
2020-10-28 23:56:51 -0500

    login , reestructuring css files

--- docs/public/js/auth.js
+++ docs/public/js/auth.js
@@ -1,60 +1,34 @@
-//signup
-const signupForm = document.querySelector('#sign-up-form');
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
 
-    //get user info
-    const email = signupForm['sign-up-email'].value;
-    const password = signupForm['sign-up-password'].value;
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
 
-    //sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred =>{
-        window.location.href='index.html';
-        signupForm.reset();
-    });
-});
 
-//logout
-const logout = document.querySelector('#logout')
-logout.addEventListener('click',(e)=>{
-    e.preventDefault();
-    auth.signOut().then(()=>{
-        console.log('user signed out')
-    });
-});
 
-// Login with Google
-const googleButton = document.querySelector("#googleLogin");
-
-googleButton.addEventListener("click", (e) => {
+const signUpForm = document.querySelector("#sign-up-form");
+signUpForm.addEventListener("submit", (e)=> {
   e.preventDefault();
-  signInForm.reset();
+  const email = signUpForm["sign-up-email"].value;
+  const password = signUpPassword["sign-up-password"].value;
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider).then((result) => {
-    console.log(result);
-    console.log("google sign in");
-  })
-  .catch(err => {
-    console.log(err);
-  })
-});
-
-// Login with Facebook
-const facebookButton = document.querySelector('#facebookLogin');
-
-facebookButton.addEventListener('click', e => {
-  e.preventDefault();
-  signInForm.reset();
-
-
-  const provider = new firebase.auth.FacebookAuthProvider();
-  auth.signInWithPopup(provider).then((result) => {
-    console.log(result);
-    console.log("facebook sign in");
-  })
-  .catch(err => {
-    console.log(err);
-  })
-
+    auth.createUserWithEmailAndPassword(email, password).then((userCredential) => {
+      signUpForm.reset();
+      
+    })
 })
+
+
+
+
+
+
+
+
